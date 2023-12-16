23a4923b29ae536fb906126af265b1aee4b77bcc
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-16 19:31:01 +0600

    separate category controller

---
+++ controller/categoryController.js
@@ -0,0 +1,56 @@
+const Category = require('../model/categortSchema');
+const SubCategory = require('../model/subCategorySchema');
+
+const categoryController = async (req, res) => {
+    try {
+        const mainCategory = new Category({
+            ...req.body
+        });
+
+        const savedMainCategory = await mainCategory.save();
+
+        if (savedMainCategory) {
+            const subCategory = new SubCategory({
+                parentId: savedMainCategory._id,
+                ...req.body
+            });
+
+            const savedSubCategory = await subCategory.save();
+
+            if (savedSubCategory) {
+                res.status(200).json({
+                    success: true,
+                    data: { mainCategory: savedMainCategory, subCategory: savedSubCategory },
+                });
+            } else {
+                res.status(404).json({ message: 'Subcategory not saved' });
+            }
+        } else {
+            res.status(404).json({ message: 'Main category not saved' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const getCategoryController = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        if (categories) {
+            res.status(200).send({
+                succes: true,
+                data: categories
+            })
+        } else {
+            res.status(404).send({ message: "Category not found" });
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+}
+
+
+module.exports = {
+    categoryController,
+    getCategoryController
+}
--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -1,60 +1,15 @@
 const express = require('express');
-const Category = require('../model/categortSchema');
-const SubCategory = require('../model/subCategorySchema');
+const { categoryController, getCategoryController } = require('../controller/categoryController');
 const categoryRouter = express.Router();
 
 
 // category post
 
-categoryRouter.post('/', async (req, res) => {
-    try {
-        const mainCategory = new Category({
-            ...req.body
-        });
-
-        const savedMainCategory = await mainCategory.save();
-
-        if (savedMainCategory) {
-            const subCategory = new SubCategory({
-                parentId: savedMainCategory._id,
-                ...req.body
-            });
-
-            const savedSubCategory = await subCategory.save();
-
-            if (savedSubCategory) {
-                res.status(200).json({
-                    success: true,
-                    data: { mainCategory: savedMainCategory, subCategory: savedSubCategory },
-                });
-            } else {
-                res.status(404).json({ message: 'Subcategory not saved' });
-            }
-        } else {
-            res.status(404).json({ message: 'Main category not saved' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+categoryRouter.post('/', categoryController);
 
 
 // category get
 
-categoryRouter.get('/', async (req, res) => {
-    try {
-        const categories = await Category.find();
-        if (categories) {
-            res.status(200).send({
-                succes: true,
-                data: categories
-            })
-        } else {
-            res.status(404).send({ message: "Category not found" });
-        }
-    } catch (error) {
-        res.status(500).send({ message: error.message });
-    }
-})
+categoryRouter.get('/', getCategoryController)
 
 module.exports = categoryRouter;
